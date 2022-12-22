aeb4d0d9e9221f0b8777f79a012286636e09d53a
Johan Carlberg <johan.hansson.carlberg@gmail.com>
2022-12-22 16:42:50 +0100

    Add home content module and initial functionality for page switching

---
+++ src/home.js
@@ -0,0 +1,45 @@
+const homeContent = document.createElement('div');
+homeContent.classList.add('home-content');
+
+const openingHoursDiv = document.createElement('div');
+const openingHoursHeader = document.createElement('h2');
+openingHoursHeader.textContent = 'Opening Hours';
+openingHoursDiv.classList.add('opening-hours');
+openingHoursDiv.appendChild(openingHoursHeader);
+
+const aboutDiv = document.createElement('div');
+aboutDiv.classList.add('about');
+const aboutHeader = document.createElement('h2');
+aboutHeader.textContent = 'About Us';
+const aboutPara = document.createElement('p');
+aboutPara.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sed deleniti cumque, quaerat eum amet qui eligendi dolores ipsa, molestias id quibusdam sapiente nam mollitia necessitatibus, rem nulla incidunt nihil ad! Odit quidem nisi eaque nulla provident officia nihil iusto?';
+aboutDiv.appendChild(aboutHeader);
+aboutDiv.appendChild(aboutPara);
+
+openingHoursDiv.appendChild(
+    createOpeningHoursItem('Monday - Friday', '11:00', '21:00')
+);
+openingHoursDiv.appendChild(
+    createOpeningHoursItem('Saturday', '15:00', '23:00')
+);
+openingHoursDiv.appendChild(
+    createOpeningHoursItem('Sunday', '11:00', '18:00')
+);
+
+function createOpeningHoursItem(days, openTime, closeTime) {
+    const openingHoursItemDiv = document.createElement('div');
+    const openingHoursItemHeader = document.createElement('h3');
+    openingHoursItemHeader.textContent = days;
+    openingHoursItemDiv.appendChild(openingHoursItemHeader);
+
+    const openingHoursItemTimes = document.createElement('span');
+    openingHoursItemTimes.textContent = `${openTime} - ${closeTime}`;
+    openingHoursItemDiv.appendChild(openingHoursItemTimes);
+
+    return openingHoursItemDiv;
+}
+
+homeContent.appendChild(openingHoursDiv);
+homeContent.appendChild(aboutDiv);
+
+export default homeContent;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import './style.css';
 import header from './header.js';
+import homeContent from './home.js';
+
 console.log('Restaurant app loaded!');
+
+const pages = {
+    'Home': homeContent,
+}
 
 document.querySelector('body').appendChild(header);
 const headerLinks = header.querySelectorAll('nav li > a');
 for (const headerLink of headerLinks) {
-    headerLink.addEventListener('click', switchTab);
+    headerLink.addEventListener('click', (e) => {
+        switchPage(e.target.textContent);
+    });
 }
 
-function switchTab(e) {
-    console.log(e.target.textContent);
+const contentDiv = document.querySelector('#content');
+switchPage('Home');
+console.log(pages);
+
+
+function switchPage(newPage) {
+    if (!pages[newPage]) {
+        console.log('switchPage: target page does not exist');
+        return;
+    }
+    // Clear all child nodes of content
+    contentDiv.innerHTML = '';
+
+    contentDiv.appendChild(pages[newPage]);
 }
